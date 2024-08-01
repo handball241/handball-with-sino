ae8aa9596176751c000a196630c7bbb69fe9c184
handball241 <yassinmo10c@gmail.com>
2024-08-01 19:43:00 +0200

    Add files via upload

---
+++ yassin.js
@@ -0,0 +1,20 @@
+alert("handball is the best")
+document.write('<center> <h1>(HANDBALL)</h1> </center>, \n ')
+
+document.write('<h2>What is handball </h2>, \n')
+
+document.write('Handball is a fast-paced team sport where two teams of seven players each (six outcourt players and a goalkeeper) pass a ball using their hands with the aim of throwing it into the goal of the other team. A standard match consists of two halves of 30 minutes, and the team that scores more goal wins., \n');
+
+document.write('<h3>rules in handball</h3>, \n')
+
+document.write('(1). **Objective**: The goal is to score more points than your opponent by throwing the ball into their goal. , \n')
+
+document.write('(2). **Game Duration**: A standard match consists of two 30-minute halves with a 15-minute break in between¹. , \n')
+
+document.write('(3). **Teams**: Each team has 7 players on the court (6 outfield players and 1 goalkeeper) and 7 substitutes¹. , \n')
+
+document.write('(4). **Ball Handling**: Players can pass, hold possession, or shoot the ball. They can dribble or take up to three steps for up to three seconds without dribbling¹. , \n')
+
+document.write('(5). **Goal Area**: Only the goalkeeper is allowed to come into contact with the floor of the goal area¹. , \n')
+
+document.write('(6). **Scoring**: A goal is scored when the ball is thrown into the opponent’s goal¹. , \n')
